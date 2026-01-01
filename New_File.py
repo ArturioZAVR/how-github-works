eccee8b5dc38fdb7a1f22c2dd60374dbd6ddb541
Arturchik <arturbabd@gmail.com>
2026-01-01 16:15:06 +0900

    Let's see
    While having branch, I tried to change main branch, what is going to happend?

--- New_File.py
+++ New_File.py
@@ -1,5 +1,6 @@
-print("This is a new file.")
 import Another_file_for_functions
+from File_called_file.is_it_folder_or_file_IDK import hello_world
+hello_world()
 
 result = Another_file_for_functions.add(5, 7)
 print(f"The result of addition is: {result}")
